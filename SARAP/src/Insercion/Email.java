d1f09b292e78953e6ae19eac9b36e59be5d40015
1FLIPPYSBOY <carlosaugustopootnovelo3ro@gmail.com>
2018-05-24 13:11:31 -0500

    mas cosas :,v

---
+++ SARAP/src/Insercion/Email.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Insercion;
+
+/**
+ *
+ * @author MS
+ */
+public class Email {
+    private String usuario;
+    private String dominio;
+
+    public Email(String usuario, String dominio) {
+        this.usuario = usuario;
+        this.dominio = dominio;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getDominio() {
+        return dominio;
+    }
+
+    public void setDominio(String dominio) {
+        this.dominio = dominio;
+    }
+    
+    
+}
