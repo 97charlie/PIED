54ac5a87615cfaed2505cc845a9a9edf4622c138
1FLIPPYSBOY <carlosaugustopootnovelo3ro@gmail.com>
2018-05-24 20:18:51 -0500

    agregadoos vision de viajes

--- SARAP/src/Insercion/vistaReservacion.java
+++ SARAP/src/Insercion/vistaReservacion.java
@@ -46,30 +46,34 @@ public class vistaReservacion extends javax.swing.JFrame {
         jTextField16 = new javax.swing.JTextField();
         jTextField17 = new javax.swing.JTextField();
         jLabel17 = new javax.swing.JLabel();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTable1 = new javax.swing.JTable();
+        jLabel2 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        setMaximumSize(new java.awt.Dimension(360, 360));
-        setMinimumSize(new java.awt.Dimension(360, 360));
-        setPreferredSize(new java.awt.Dimension(360, 360));
-        setSize(new java.awt.Dimension(360, 360));
+        setBounds(new java.awt.Rectangle(0, 0, 0, 0));
+        setMaximumSize(new java.awt.Dimension(480, 600));
+        setMinimumSize(new java.awt.Dimension(480, 600));
+        setPreferredSize(new java.awt.Dimension(480, 600));
+        setSize(new java.awt.Dimension(480, 600));
         getContentPane().setLayout(null);
 
         jLabel1.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
-        jLabel1.setText("Reservacion");
+        jLabel1.setText("Viajes");
         getContentPane().add(jLabel1);
-        jLabel1.setBounds(10, 10, 150, 30);
+        jLabel1.setBounds(20, 250, 80, 30);
 
         jButton2.setText("Agregar");
         getContentPane().add(jButton2);
-        jButton2.setBounds(30, 270, 79, 25);
+        jButton2.setBounds(20, 210, 79, 25);
 
         jButton1.setText("Eliminar");
         getContentPane().add(jButton1);
-        jButton1.setBounds(120, 270, 79, 25);
+        jButton1.setBounds(110, 210, 79, 25);
 
         jButton3.setText("Limpiar");
         getContentPane().add(jButton3);
-        jButton3.setBounds(210, 270, 75, 25);
+        jButton3.setBounds(200, 210, 75, 25);
 
         jTextField19.setToolTipText("");
         getContentPane().add(jTextField19);
@@ -121,15 +125,52 @@ public class vistaReservacion extends javax.swing.JFrame {
 
         jTextField16.setToolTipText("");
         getContentPane().add(jTextField16);
-        jTextField16.setBounds(70, 220, 40, 22);
+        jTextField16.setBounds(390, 160, 40, 22);
 
         jTextField17.setToolTipText("");
         getContentPane().add(jTextField17);
-        jTextField17.setBounds(30, 220, 40, 22);
+        jTextField17.setBounds(350, 160, 40, 22);
 
         jLabel17.setText("Hora Llegada");
         getContentPane().add(jLabel17);
-        jLabel17.setBounds(30, 200, 80, 20);
+        jLabel17.setBounds(350, 140, 80, 20);
+
+        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null}
+            },
+            new String [] {
+                "Origen", "Destino", "Salida", "Llegada", "Precio", "Marca"
+            }
+        ));
+        jScrollPane1.setViewportView(jTable1);
+
+        getContentPane().add(jScrollPane1);
+        jScrollPane1.setBounds(20, 290, 430, 240);
+
+        jLabel2.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
+        jLabel2.setText("Reservacion");
+        getContentPane().add(jLabel2);
+        jLabel2.setBounds(10, 10, 150, 30);
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
@@ -176,9 +217,12 @@ public class vistaReservacion extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel10;
     private javax.swing.JLabel jLabel17;
+    private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel20;
     private javax.swing.JLabel jLabel21;
     private javax.swing.JLabel jLabel9;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTable jTable1;
     private javax.swing.JTextField jTextField10;
     private javax.swing.JTextField jTextField11;
     private javax.swing.JTextField jTextField12;
